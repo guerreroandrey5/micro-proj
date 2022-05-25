f4b80416f50c4cd098c937c5965a240ac40fd92b
Git <git@github.com>
2022-05-25 15:55:50 -0600

    LOLOGREE

--- parkeo/parkeo/Program.cs
+++ parkeo/parkeo/Program.cs
@@ -8,48 +8,99 @@ namespace Micro_Proyecto
 {
     class Program
     {
+        static string[,] data = new string[10, 5];
+        static string[,] parkinglot = new string[10, 5];
         static void Main(string[] args)
         {
             Console.WriteLine("Ingrese una opcion:\n1-Parkeo");
             int sel = int.Parse(Console.ReadLine());
+            park();
             switch (sel)
             {
                 case 1:
-                    park();
-                    //ME cago en la puta por si acaso
+                    registrar();
                     break;
                 default:
                     break;
             }
         }
+
         static void park()
         {
-            string[,] parkinglot = new string[10, 5];
+           
             string[] letras = { "A", "B", "C", "D", "E" };
-            for (int f = 0; f < 10; f ++)
+            
+            for (int f = 0; f < 10; f++)
+                
             {
                 for (int c = 0; c < 5; c++)
                 {
                     string linea;
-                    linea = letras[c] + (f);
+                    if (f == 9)
+                    {
+                       linea = letras[c] + (f + 1) + "    ";
+                    }
+                    else
+                    {
+                        linea = letras[c] + (f + 1) + "     ";
+                       
+                    }
                     parkinglot[f, c] = (linea);
                 }
             }
-            print(parkinglot);
+            
         }
-        static void print(string[,] park)
+
+        static void registrar()
         {
+            for (int f = 0; f < 10; f++)
             {
-                for (int f = 0; f < 10; f++)
+                for (int c = 0; c < 5; c++)
                 {
-                    for (int c = 0; c < 5; c++)
+                    if (data[f,c] == null)
                     {
-                        Console.Write(park[f, c] + "  ");
+                        print(parkinglot[f, c], ConsoleColor.Green);
+                    } else
+                    {
+                        print(parkinglot[f, c], ConsoleColor.Red);
                     }
-                    Console.WriteLine();
                 }
-                Console.ReadKey();
+                Console.WriteLine();
             }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Seleccione el campo que desea usar: ");
+            string campo = Console.ReadLine();
+            int[] adsdas = {1,2,3 }; 
+            if(comprobar(parkinglot, campo))
+            {
+                Console.WriteLine("YES");
+            } else
+            {
+                Console.WriteLine("Mamaste");
+            }
+        }
+
+        static bool comprobar(string[,] matriz, string dato)
+        {
+            bool check = false;
+
+            foreach (string item in matriz)
+            {
+               if(item == dato)
+                {
+                    return true;
+                }
+            }
+
+            return check;
+        }
+
+        static void print(string park, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(park + "  ");
+     
+          
         }
     }
 }
