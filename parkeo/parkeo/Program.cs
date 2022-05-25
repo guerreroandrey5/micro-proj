7cf8afe5fb972f0818fa7c5d48eb6ee45e553c6b
guerreroandrey5 <guerreroandrey@gmail.com>
2022-05-25 14:19:01 -0600

    code2

--- parkeo/parkeo/Program.cs
+++ parkeo/parkeo/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace parkeo
+namespace Micro_Proyecto
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese una opcion:\n1-Parkeo");
+            int sel = int.Parse(Console.ReadLine());
+            switch (sel)
+            {
+                case 1:
+                    park();
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void park()
+        {
+            string[,] parkinglot = new string[50, 10];
+            print(parkinglot);
+            //Console.WriteLine(parkinglot);
+        }
+        static void print(string[,] park)
+        {
+            Console.WriteLine("<=======================>");
+            foreach (var val in park)
+            {
+                Console.WriteLine(val);
+            }
         }
     }
 }
